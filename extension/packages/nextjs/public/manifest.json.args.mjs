2f4ab82f301d4eb897dc23607cc6b9aa4feefb28
port <108868128+portdeveloper@users.noreply.github.com>
2025-05-28 14:24:23 +0300

    Change extraContents to extraContent

--- extension/packages/nextjs/public/manifest.json.args.mjs
+++ extension/packages/nextjs/public/manifest.json.args.mjs
@@ -1,24 +1,24 @@
-export const name = "Scaffold-ETH 2 + Serwist"
-export const description = "A Progressive Web App built with Scaffold-ETH 2"
-export const iconPath = "" // allows to omit default SE-2 icons in manifest.json
-export const extraContents = {
+export const name = "Scaffold-ETH 2 + Serwist";
+export const description = "A Progressive Web App built with Scaffold-ETH 2";
+export const iconPath = ""; // allows to omit default SE-2 icons in manifest.json
+export const extraContent = {
   short_name: "Scaffold-Serwist",
-    display: "standalone",
-    orientation: "portrait",
-    theme_color: "#FFFFFF",
-    background_color: "#FFFFFF",
-    start_url: "/",
-    icons: [
-      {
-        src: "/icons/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any"
-      },
-      {
-        src: "/icons/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png"
-      }
-  ]
-}
+  display: "standalone",
+  orientation: "portrait",
+  theme_color: "#FFFFFF",
+  background_color: "#FFFFFF",
+  start_url: "/",
+  icons: [
+    {
+      src: "/icons/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "any",
+    },
+    {
+      src: "/icons/icon-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
+};
