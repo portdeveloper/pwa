a553e543af925cc91d70f9ca8c4c43bf88270fb9
port <108868128+portdeveloper@users.noreply.github.com>
2025-05-28 14:43:40 +0300

    Remove import next config from next.config.ts.args.mjs

--- extension/packages/nextjs/next.config.ts.args.mjs
+++ extension/packages/nextjs/next.config.ts.args.mjs
@@ -1,6 +1,5 @@
 export const preConfigContent = `
 import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from "next/constants.js";
-import type { NextConfig } from "next";
 `
 
 export const configOverrides = {}
