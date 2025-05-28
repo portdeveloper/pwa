1cc9ca48c1b3d9cec335942ef9b75c6dff4c620e
port <108868128+portdeveloper@users.noreply.github.com>
2025-05-28 12:23:21 +0300

    Fix .env.example.args.mjs

--- extension/packages/nextjs/.env.example.args.mjs
+++ extension/packages/nextjs/.env.example.args.mjs
@@ -1,7 +1,5 @@
-export default {
-  additionalVars: [
-    "WEB_PUSH_EMAIL=",
-    "WEB_PUSH_PRIVATE_KEY=",
-    "NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY="
-  ]
-};
+export const additionalVars = [
+  "WEB_PUSH_EMAIL=",
+  "WEB_PUSH_PRIVATE_KEY=",
+  "NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY=",
+];
